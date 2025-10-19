bdb6937871272045182bea70049e462279e44aff
Valerii Melnyk <valeriimelnyk0@gmail.com>
2025-10-19 10:29:46 +0200

    feat: Add homework for module 0

---
+++ tests/test_module_0.py
@@ -0,0 +1,12 @@
+Fruit_name = ["Apple" , "Pineapple" , "Mango" , "Banana" , "Orange"]
+print(Fruit_name[2])
+user_age = 29
+if user_age >=  18:
+    print(f"user_age is greater than or equal to 18 exit" "access is allowed")
+else:
+    print(f"user_age is less than 18 exit" "access is denied")
+
+user_name = "Valerii"
+User_surname = "Melnyk"
+Full_name = "Valerii Melnyk"
+print(Full_name)
